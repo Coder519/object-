47bdd1d2d624fad5cd59e04bdabb42608d7b4031
Rachit tambi <95204792+Coder519@users.noreply.github.com>
2022-08-11 12:52:46 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 img ="";
 status = "";
+objects= [];
 function preload(){
           img = loadImage('dog_cat.jpg');
 }
 function setup(){
           canvas = createCanvas(640 , 420);
           canvas.center();
-          objectDetector = ml5.objectDetector('cocossd' . modelLoaded)
+          objectDetector = ml5.objectDetector('cocossd' , modelLoaded)
           document.getElementById("status").innerHTML = "Status : Detecting Objets";
 }
 function modelLoaded(){
@@ -20,15 +21,23 @@ function gotResult(error, results){
                     console.log(error)
           }
           console.log(results);
+          objects = results;
 }
 
 function draw(){
           image(img, 0,0,640,420);
-          fill("#F0000");
-          text("Dog",45,45);
-          text("Cat",500,300,);
-          noFill();
-          stroke("#F0000");
-          rect(30,60,450,350);
-          rect(250,40,500,300)
+          if(status !="")
+          {
+              for (i = 0; i< objects.length; i++)
+              {
+                        document.getElementById("status").innerHTML = "Status : Object Detected";
+
+                        fill("#FF0000")
+                        percent =floor(objects[i].confidence *100);
+                        text(objects[i].label+" "+percent+"%",objects[i].x,objects[i].y);
+                        noFill();
+                        stroke("#FF0000");
+                        rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+              }      
+          }
 }
